b2045670645cf74adde6b84810121dce513d4dd3
Oluwasegun Thomas (Brainiac) <brainiacerudite@gmail.com>
2026-01-21 23:05:57 +0100

    added jest package and config

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest"
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/**/*.test.ts'],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true,
+    moduleDirectories: ['node_modules', 'src'],
+}
+
+export default config
